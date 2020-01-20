a1bf7ff66ba62b67da991557e49c66c52c45bba6
NatureFreshMilk <naturefreshmilk@github.com>
2020-01-20 13:57:59 +0100

    discord stub

--- src/handler/discord.js
+++ src/handler/discord.js
@@ -1,4 +1,20 @@
+const Discord = require('discord.js');
 
-module.exports = function(){
+module.exports = function(remote, events){
+  Object.keys(remote.channels).forEach(ingame_channel => {
+    const token = remote.channels[ingame_channel];
+    const client = new Discord.Client();
 
+    client.on('ready', () => {
+      console.log(`Logged in as ${client.user.tag}!`);
+    });
+
+    client.on('message', msg => {
+      if (msg.content === 'ping') {
+        msg.reply('Pong!');
+      }
+    });
+
+    client.login(token);    
+  });
 }
